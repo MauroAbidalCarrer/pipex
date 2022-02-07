f729f356770b987a1a546346d687f4cfa7fddfb2
Mauro ABIDAL CARRER <maabidal@e2r10p18.clusters.42paris.fr>
2022-02-07 23:30:34 +0100

    c bon la?

--- Makefile
+++ Makefile
@@ -6,44 +6,44 @@
 #    By: maabidal <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/29 20:08:50 by maabidal          #+#    #+#              #
-#    Updated: 2022/02/07 22:56:58 by maabidal         ###   ########.fr        #
+#    Updated: 2022/02/07 23:29:47 by maabidal         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+MANDATORY_MAIN	=	main.c
+
 SRCS 		=	cmd_utils.c\
-				main.c\
-				process.c\
-				sys_calls.c\
-				utils.c
+			process.c\
+			sys_calls.c\
+			utils.c
 
 BONUS_SRCS 	=	get_next_line_bonus.c\
-				get_next_line_bonus.h\
-				get_next_line_utils_bonus.c\
-				main_bonus.c
+			get_next_line_utils_bonus.c\
+			main_bonus.c
 
 NAME	= pipex
 CFLAGS	= -Wall -Wextra -Werror
 RM	= rm -f
 OBJS	= $(SRCS:.c=.o)
 BNSOBJS	= $(BONUS_SRCS:.c=.o)
+MAINOBJS= $(MANDATORY_MAIN:.c=.o)
 
 all : $(NAME)
 
-bonus : $(BNSOBJS)
-	$(CC) -o $(NAME) $(BNSOBJS)
+bonus : $(BNSOBJS) $(OBJS)
+	$(CC) -o $(NAME) $(BNSOBJS) $(OBJS)
 
-$(NAME) : $(OBJS)
-	$(CC) -o $(NAME) $(OBJS)
+$(NAME) : $(OBJS) $(MAINOBJS)
+	$(CC) -o $(NAME) $(OBJS) $(MAINOBJS)
 
 .o : .c
 	$(CC) $(CFLAGS) -c $< -o $@ -I ./
 
 clean : 
-	$(RM) $(OBJS) $(BNSOBJS)
+	$(RM) $(OBJS) $(BNSOBJS) $(MAINOBJS)
 
 fclean : clean
 	$(RM) $(NAME)
-	$(RM) $(BNS_NAME)
 
 re :    fclean all
 
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,31 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maabidal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/04 21:25:15 by maabidal          #+#    #+#             */
-/*   Updated: 2022/02/05 17:16:54 by maabidal         ###   ########.fr       */
+/*   Created: 2021/12/06 18:27:44 by maabidal          #+#    #+#             */
+/*   Updated: 2022/01/04 19:38:40 by maabidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "header.h"
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
-int	main(int ac, char **av, char **env)
-{
-	int	p_fds[2];
+# include<unistd.h>
+# include<limits.h>
+# include<stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
-	if (ac != 5)
-	{
-		write(2, "pipex: Error: nb of arguments isn't 4\n", 38);
-		return (1);
-	}
-	ft_pipe(p_fds);
-	if (ft_fork() == 0)
-	{
-		ft_close(p_fds[0]);
-		exe_first(av[2], av[1], env, p_fds[1]);
-	}
-	exe_last(av[3], av[4], env, p_fds);
-}
+void	*free_prev(char **prev_add);
+char	*get_next_line(int fd);
+char	*join(char *s1, char *s2);
+size_t	ft_strlen(char *str);
+char	*r_join(char *s1, char *s2, size_t len);
+char	*alloc_line(size_t len);
+size_t	n_index(char *str);
+#endif
